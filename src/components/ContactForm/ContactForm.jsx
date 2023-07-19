6d4fc70693768144bd38b0badda8daffa91cea65
Szymon Oponczewski <szymon_o@mac.com>
2023-07-19 19:05:42 +0200

    added validation pattern for inputs in ContactForm

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -22,6 +22,7 @@ export const ContactForm = () => {
           <input
             type="text"
             name="name"
+            pattern="^[A-Za-z.' \-]+$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             required
             className={css.formInput}
@@ -33,6 +34,7 @@ export const ContactForm = () => {
           <input
             type="tel"
             name="number"
+            pattern="^\+?\d{1,4}?\s?\(?\d{1,4}?\)?\s?\d{1,4}\s?\d{1,4}\s?\d{1,9}$"
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
             required
             className={css.formInput}
